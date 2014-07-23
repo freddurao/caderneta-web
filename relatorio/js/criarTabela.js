4a286d07ac63de772ff3d466f80ae665fc74f00f
ygormutti <ygormutti@e9ba441c-933c-e703-e28b-2eab9300b162>
2014-07-23 05:51:02 +0000

    Alteração no template do relatório enviada por Ester em 7/7

--- relatorio/js/criarTabela.js
+++ relatorio/js/criarTabela.js
@@ -1,8 +1,16 @@
 
 function criarTabela(){
-	var i 
-	for (i=1;i<=34;i++) { 
-		//document.write(teste) 
-		alert("teste")
-	} 
+	// Captura a referência da tabela com id “minhaTabela”
+	var table = document.getElementById("minhaTabela");
+
+	for(var i = 0; i < 3; i++)
+	{
+		var newRow = table.insertRow(i);
+		for(var j = 0; j < 34; j++) 
+		{
+			newCell = newRow.insertCell(j);
+			newCell.innerHTML = "Linha "+ numOfRows + " – Coluna "+ j;
+		}
+		
+	}
 }
